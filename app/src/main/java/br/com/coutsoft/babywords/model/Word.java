f63a6a435e085ebc14524a6c789d30a50ed153ae
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-15 21:44:44 -0300

    create model class Word

---
+++ app/src/main/java/br/com/coutsoft/babywords/model/Word.java
@@ -0,0 +1,17 @@
+package br.com.coutsoft.babywords.model;
+
+public class Word {
+    private String name;
+
+    public Word(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

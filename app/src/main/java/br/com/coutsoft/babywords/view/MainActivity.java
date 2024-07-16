2dbf36bfbc7756b985025bd1fae5f3b42e5c3727
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-16 14:33:11 -0300

    include adapter call to MainActivity

--- app/src/main/java/br/com/coutsoft/babywords/view/MainActivity.java
+++ app/src/main/java/br/com/coutsoft/babywords/view/MainActivity.java
@@ -3,14 +3,37 @@ package br.com.coutsoft.babywords.view;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.coutsoft.babywords.R;
+import br.com.coutsoft.babywords.model.Word;
+import br.com.coutsoft.babywords.service.WordAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private WordAdapter adapter;
+    private List<Word> wordList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        wordList = new ArrayList<>();
+        wordList.add(new Word("Apple", LocalDate.of(2024, 6, 16)));
+        wordList.add(new Word("Banana", LocalDate.of(2024, 5, 22)));
+        wordList.add(new Word("Orange", LocalDate.of(2024, 4, 26)));
+
+        adapter = new WordAdapter(wordList);
+        recyclerView.setAdapter(adapter);
     }
 }

482e0dedf68863d6a059fc7236dd1a482f82ae96
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-15 21:47:49 -0300

    add field date to Word

--- app/src/main/java/br/com/coutsoft/babywords/model/Word.java
+++ app/src/main/java/br/com/coutsoft/babywords/model/Word.java
@@ -1,10 +1,14 @@
 package br.com.coutsoft.babywords.model;
 
+import java.time.LocalDate;
+
 public class Word {
     private String name;
+    private LocalDate date;
 
-    public Word(String name) {
+    public Word(String name, LocalDate date) {
         this.name = name;
+        this.date = date;
     }
 
     public String getName() {
@@ -13,5 +17,13 @@ public class Word {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 }
